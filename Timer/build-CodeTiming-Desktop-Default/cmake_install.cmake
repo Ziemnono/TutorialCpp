3f52f6babb42e007f90cab9d834fb93033bcca7c
Arnaud <amazier>
2019-06-10 17:45:38 +0200

    cleanup of builds due to modification of path files

--- Timer/build-CodeTiming-Desktop-Default/cmake_install.cmake
+++ Timer/build-CodeTiming-Desktop-Default/cmake_install.cmake
@@ -1,4 +1,4 @@
-# Install script for directory: /home/arnaud/Desktop/C++/Timer/CodeTiming
+# Install script for directory: /home/arnaud/dev/TutorialCpp/Timer/CodeTiming
 
 # Set the install prefix
 if(NOT DEFINED CMAKE_INSTALL_PREFIX)
@@ -45,5 +45,5 @@ endif()
 
 string(REPLACE ";" "\n" CMAKE_INSTALL_MANIFEST_CONTENT
        "${CMAKE_INSTALL_MANIFEST_FILES}")
-file(WRITE "/home/arnaud/Desktop/C++/Timer/build-CodeTiming-Desktop-Default/${CMAKE_INSTALL_MANIFEST}"
+file(WRITE "/home/arnaud/dev/TutorialCpp/Timer/build-CodeTiming-Desktop-Default/${CMAKE_INSTALL_MANIFEST}"
      "${CMAKE_INSTALL_MANIFEST_CONTENT}")
